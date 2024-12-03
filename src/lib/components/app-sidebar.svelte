6b49ad2f6ead014d976eb30fb1edbaa176858e49
Sylvie Rupe <raygenrrupe@gmail.com>
2024-12-03 15:02:43 -0700

    eeeee

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,43 +1,72 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition';
-	import Calendar from 'lucide-svelte/icons/calendar';
-	import House from 'lucide-svelte/icons/house';
-	import Inbox from 'lucide-svelte/icons/inbox';
-	import Search from 'lucide-svelte/icons/search';
-	import Settings from 'lucide-svelte/icons/settings';
-	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
+	import {
+		House,
+		Beaker,
+		Shapes,
+		Dices,
+		Laugh,
+		Calculator,
+		CircleHelp,
+		HousePlus,
+		LoaderPinwheel,
+		TestTubeDiagonal
+	} from 'lucide-svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import { onMount } from 'svelte';
-	const sidebar = useSidebar();
+	import * as Collapsible from '$lib/components/ui/collapsible/index.js';
+	import { slide } from 'svelte/transition';
 
-	let triggerIcon = House;
-
-	// Menu items.
-	const items = [
+	const navMain = [
 		{
-			title: 'Home',
-			url: '#',
+			title: 'About Me',
+			url: 'about',
 			icon: House
 		},
 		{
-			title: 'Inbox',
+			title: 'Experiences',
 			url: '#',
-			icon: Inbox
+			icon: Beaker,
+			items: [
+				{
+					title: 'Quilting',
+					url: '#',
+					icon: Shapes
+				},
+				{
+					title: 'Tabletop Game Design',
+					url: '#',
+					icon: Dices
+				}
+			]
 		},
 		{
-			title: 'Calendar',
+			title: 'XLAB',
 			url: '#',
-			icon: Calendar
+			icon: LoaderPinwheel
 		},
 		{
-			title: 'Search',
+			title: 'Project Good',
 			url: '#',
-			icon: Search
+			icon: Laugh
 		},
 		{
-			title: 'Settings',
+			title: 'Math',
 			url: '#',
-			icon: Settings
+			icon: Calculator
+		},
+		{
+			title: 'Living in Beta',
+			url: '#',
+			icon: HousePlus
+		},
+		{
+			title: 'Flex Lab',
+			url: '#',
+			icon: TestTubeDiagonal
+		},
+		{
+			title: 'Miscellaneous Info',
+			url: '#',
+			icon: CircleHelp
 		}
 	];
 </script>
@@ -48,17 +77,51 @@
 			<Sidebar.GroupLabel>Portfolio</Sidebar.GroupLabel>
 			<Sidebar.GroupContent>
 				<Sidebar.Menu>
-					{#each items as item (item.title)}
-						<Sidebar.MenuItem>
-							<Sidebar.MenuButton>
-								{#snippet child({ props })}
-									<a href={item.url} {...props}>
-										<item.icon />
-										<span>{item.title}</span>
-									</a>
-								{/snippet}
-							</Sidebar.MenuButton>
-						</Sidebar.MenuItem>
+					{#each navMain as item (item.title)}
+						{#if item.items}
+							<Collapsible.Root open class="group/collapsible">
+								<Sidebar.MenuItem>
+									<Collapsible.Trigger>
+										{#snippet child({ props })}
+											<Sidebar.MenuButton {...props}>
+												<item.icon />
+												<span>{item.title}</span>
+											</Sidebar.MenuButton>
+										{/snippet}
+									</Collapsible.Trigger>
+									<Collapsible.Content>
+										<Sidebar.MenuSub>
+											<div transition:slide>
+												<!-- what ^  -->
+												{#each item.items as subItem (subItem.title)}
+													<Sidebar.MenuSubItem>
+														<Sidebar.MenuSubButton>
+															{#snippet child({ props })}
+																<a href={subItem.url} {...props}>
+																	<subItem.icon />
+																	<span>{subItem.title}</span>
+																</a>
+															{/snippet}
+														</Sidebar.MenuSubButton>
+													</Sidebar.MenuSubItem>
+												{/each}
+											</div>
+										</Sidebar.MenuSub>
+									</Collapsible.Content>
+								</Sidebar.MenuItem>
+							</Collapsible.Root>
+						{:else}
+							<Sidebar.MenuItem>
+								<Sidebar.MenuButton>
+									{#snippet child({ props })}
+										<a href={item.url} {...props}>
+											<item.icon />
+											<span>{item.title}</span>
+										</a>
+									{/snippet}
+								</Sidebar.MenuButton>
+							</Sidebar.MenuItem>
+						{/if}
 					{/each}
 				</Sidebar.Menu>
 			</Sidebar.GroupContent>
