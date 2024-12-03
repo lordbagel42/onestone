6ad9bc1e6210e2315f99870ef9b42ee8271134f2
Sylvie Rupe <raygenrrupe@gmail.com>
2024-12-03 12:33:55 -0700

    circle

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import Calendar from 'lucide-svelte/icons/calendar';
 	import House from 'lucide-svelte/icons/house';
 	import Inbox from 'lucide-svelte/icons/inbox';
 	import Search from 'lucide-svelte/icons/search';
 	import Settings from 'lucide-svelte/icons/settings';
+	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import { onMount } from 'svelte';
+	const sidebar = useSidebar();
+
+	let triggerIcon = House;
 
 	// Menu items.
 	const items = [
@@ -36,10 +42,10 @@
 	];
 </script>
 
-<Sidebar.Root variant="floating" collapsible="icon">
+<Sidebar.Root variant="inset" collapsible="icon">
 	<Sidebar.Content>
 		<Sidebar.Group>
-			<Sidebar.GroupLabel>Application</Sidebar.GroupLabel>
+			<Sidebar.GroupLabel>Portfolio</Sidebar.GroupLabel>
 			<Sidebar.GroupContent>
 				<Sidebar.Menu>
 					{#each items as item (item.title)}
